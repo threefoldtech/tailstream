022e441966f81f830979365739b4b83697cad7be
Muhamad Azamy <muhamad@incubaid.com>
2022-04-28 13:17:01 +0200

    proper redis pooling

--- src/output/redis.rs
+++ src/output/redis.rs
@@ -1,21 +1,25 @@
 use anyhow::{Context, Result};
-use r2d2::ManageConnection;
-use redis::{cmd, Client};
+use r2d2::Pool;
+use redis::{Client, Commands};
 use std::io::{Error, ErrorKind, Write};
 
 pub struct Redis {
-    client: Client,
+    pool: Pool<Client>,
     channel: String,
 }
 
 impl Redis {
     pub fn new<U: AsRef<str>, C: Into<String>>(url: U, channel: C) -> Result<Redis> {
         //let info: redis::ConnectionInfo = url.as_ref().parse()?;
-
         let client = Client::open(url.as_ref()).context("failed to create redis connection")?;
+        let pool = Pool::builder()
+            .max_size(4)
+            .idle_timeout(Some(std::time::Duration::from_secs(5 * 60)))
+            .build(client)
+            .context("failed to create redis pool")?;
 
         Ok(Redis {
-            client,
+            pool,
             channel: channel.into(),
         })
     }
@@ -23,18 +27,14 @@ impl Redis {
 
 impl Write for Redis {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let mut client = match self.client.connect() {
+        let mut client = match self.pool.get() {
             Ok(client) => client,
             Err(err) => {
                 return Err(Error::new(ErrorKind::ConnectionRefused, err));
             }
         };
 
-        match cmd("PUBLISH")
-            .arg(&self.channel)
-            .arg(buf)
-            .query(&mut client)
-        {
+        match client.publish(&self.channel, buf) {
             Ok(()) => Ok(buf.len()),
             Err(err) => Err(Error::new(ErrorKind::Other, err)),
         }
