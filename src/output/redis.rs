37711c3dc6218abb0db89b6a3b39ac714c361241
OmarElawady <omarelawady1998@gmail.com>
2022-05-30 12:10:32 +0200

    strip square brackets of ipv6 after parsing (#2)
    * strip square brackets of ipv6 after parsing
    
    * add some tests for url parsing

--- src/output/redis.rs
+++ src/output/redis.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use r2d2::Pool;
+use redis::ConnectionAddr::Tcp;
 use redis::{Client, Commands};
 use std::io::{Error, ErrorKind, Write};
 
@@ -9,9 +10,18 @@ pub struct Redis {
 }
 
 impl Redis {
+    fn init_client<U: AsRef<str>>(url: U) -> Result<Client> {
+        let mut info: redis::ConnectionInfo =
+            url.as_ref().parse().context("failed to parse redis url")?;
+        if let Tcp(ref ip, ref port) = &info.addr {
+            // url parse returns ipv6 surrounded by []
+            // net's TcpStream (used by the client) doesn't work with it surrounded
+            info.addr = Tcp(ip.trim_matches(&['[', ']'] as &[char]).to_string(), *port);
+        }
+        Client::open(info).context("failed to create redis connection")
+    }
     pub fn new<U: AsRef<str>, C: Into<String>>(url: U, channel: C) -> Result<Redis> {
-        //let info: redis::ConnectionInfo = url.as_ref().parse()?;
-        let client = Client::open(url.as_ref()).context("failed to create redis connection")?;
+        let client = Self::init_client(url)?;
         let pool = Pool::builder()
             .max_size(1)
             .idle_timeout(Some(std::time::Duration::from_secs(5 * 60)))
@@ -42,5 +52,43 @@ impl Write for Redis {
 
     fn flush(&mut self) -> std::io::Result<()> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tcp() {
+        let protocols = ["redis"];
+        let auths = ["", "user:password@"];
+        let ports = [":6379", ""];
+        let hosts = ["127.0.0.1", "[::1]", "hamada.com"];
+        for prot in protocols {
+            for auth in auths {
+                for port in ports {
+                    for host in hosts {
+                        let conn_string = format!("{}://{}{}{}", prot, auth, host, port);
+                        let client = Redis::init_client(conn_string.clone())
+                            .expect(format!("{} failed", conn_string).as_str());
+                        let conn = client.get_connection_info();
+                        let stripped_host = host.trim_matches(&['[', ']'] as &[char]);
+                        let port = if port == "" { ":6379" } else { port };
+                        assert!(conn.addr.to_string() == format!("{}{}", stripped_host, port));
+                        if auth != "" {
+                            assert!(conn.redis.username == Some("user".to_string()));
+                            assert!(conn.redis.password == Some("password".to_string()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    #[test]
+    fn test_unix() {
+        let client = Redis::init_client("unix:///var/run/redis.sock").expect("unix");
+        let conn = client.get_connection_info();
+        assert!(conn.addr.to_string() == "/var/run/redis.sock");
     }
 }
